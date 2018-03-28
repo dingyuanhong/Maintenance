a6a0e523e35d31cfb7bcd59d0ed4b7afb5d56b7c
yhding <ilooyo2014fz>
2018-03-28 20:09:49 +0800

    提交g++配置

--- InstallScript/g++.sh
+++ InstallScript/g++.sh
@@ -20,6 +20,7 @@ make install
 #/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-unknown-linux-gnu-gcc 52
 #/usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-unknown-linux-gnu-c++ 52
 
-/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-pc-linux-gnu-g++ 72
-/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-pc-linux-gnu-g++ 72
+/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-pc-linux-gnu-gcc-7.2.0 72
+/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-pc-linux-gnu-gcc 72
 /usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-pc-linux-gnu-c++ 72
+/usr/sbin/update-alternatives --install  /usr/bin/g++ g++ /usr/local/bin/x86_64-pc-linux-gnu-g++ 72
