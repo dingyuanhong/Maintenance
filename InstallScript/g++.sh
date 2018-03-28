5ebf1a97a64baa6b1851707f4b2c431cac850b26
yhding <ilooyo2014fz>
2018-03-28 11:40:07 +0800

    提交g++配置脚本

---
+++ InstallScript/g++.sh
@@ -0,0 +1,17 @@
+#wget http://gcc.parentingamerica.com/releases/gcc-5.2.0/gcc-5.2.0.tar.bz2
+wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.bz2
+
+tar -xf gcc-5.2.0.tar.bz2
+cd gcc-5.2.0
+#œ¬‘ÿ“¿¿µ
+./contrib/download_prerequisites
+
+#±‡“Î
+./configure --enable-checking=release --enable-languages=c,c++ --disable-multilib
+make
+make install
+
+#…˝º∂gcc
+/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-unknown-linux-gnu-gcc-5.2.0 52
+/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-unknown-linux-gnu-gcc 52
+/usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-unknown-linux-gnu-c++ 52
