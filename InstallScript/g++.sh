5656bec15b8158f7f5aec6df39560a722227e5d3
yhding <ilooyo2014fz>
2018-03-28 19:58:42 +0800

    提交g++脚本配置

--- InstallScript/g++.sh
+++ InstallScript/g++.sh
@@ -1,8 +1,12 @@
 #wget http://gcc.parentingamerica.com/releases/gcc-5.2.0/gcc-5.2.0.tar.bz2
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.bz2
+#wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.bz2
+#tar -xf gcc-5.2.0.tar.bz2
+#cd gcc-5.2.0
 
-tar -xf gcc-5.2.0.tar.bz2
-cd gcc-5.2.0
+wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-7.2.0/gcc-7.2.0.tar.gz
+tar -xf gcc-7.2.0.tar.gz
+cd gcc-7.2.0
+
 #œ¬‘ÿ“¿¿µ
 ./contrib/download_prerequisites
 
@@ -12,6 +16,10 @@ make
 make install
 
 #…˝º∂gcc
-/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-unknown-linux-gnu-gcc-5.2.0 52
-/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-unknown-linux-gnu-gcc 52
-/usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-unknown-linux-gnu-c++ 52
+#/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-unknown-linux-gnu-gcc-5.2.0 52
+#/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-unknown-linux-gnu-gcc 52
+#/usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-unknown-linux-gnu-c++ 52
+
+/usr/sbin/update-alternatives --install  /usr/bin/gcc gcc /usr/local/bin/x86_64-pc-linux-gnu-g++ 72
+/usr/sbin/update-alternatives --install  /usr/bin/cc cc /usr/local/bin/x86_64-pc-linux-gnu-g++ 72
+/usr/sbin/update-alternatives --install  /usr/bin/c++ c++ /usr/local/bin/x86_64-pc-linux-gnu-c++ 72
